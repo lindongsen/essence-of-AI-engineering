6632a5288c0830ddf60c62749bae85017d82baea
DawsonLin <lin_dongsen@126.com>
2025-10-22 00:26:23 +0800

    refactor: cmd_tool.exec_cmd, some ways to ignore stderr

--- tools/cmd_tool.py
+++ tools/cmd_tool.py
@@ -24,21 +24,53 @@ def build_env(d:dict=None):
         env.update(d)
     return env
 
-def exec_cmd(cmd_string):
+def _format_return(t:tuple):
+    """ truncate text for stdout and stderr """
+    return (t[0], format_text(t[1]), format_text(t[2]))
+
+def format_return(cmd_string:str, t:tuple):
+    """ hack result """
+    # no need stderr
+    for tool in [
+        "curl", "wget",
+        "uv add", "uv sync",
+        "pip install",
+    ]:
+        if not t[2]:
+            break
+        if cmd_string.startswith(tool + " "):
+            t = (t[0], t[1], "")
+            return _format_return(t)
+
+    return _format_return(t)
+
+def exec_cmd(cmd_string, no_need_stderr=False):
     """
     # parameters
     :cmd_string: cmd line.
+    :no_need_stderr: bool; if True, stderr will be null string; default is False for raw content.
 
     # return
     tuple(code, stdout, stderr)
     """
     env = build_env()
+    t = None
     try:
         result = subprocess.run(cmd_string, env=env, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=False)
         # return tuple(returncode, stdout, stderr)
-        return (result.returncode, format_text(result.stdout), format_text(result.stderr))
+        t = (
+            result.returncode,
+            result.stdout,
+            "" if no_need_stderr else result.stderr
+        )
     except subprocess.CalledProcessError as e:
-        return (e.returncode, "", format_text(e.stderr))
+        t = (
+            e.returncode,
+            str(e),
+            "" if no_need_stderr else e.stderr
+        )
+
+    return format_return(cmd_string, t)
 
 # name: func
 TOOLS = dict(
