b74ac450ed1b8a483b517aabf78eac10f81fe014
DawsonLin <lin_dongsen@126.com>
2025-11-02 00:49:40 +0800

    refactor for message truncated

--- tools/cmd_tool.py
+++ tools/cmd_tool.py
@@ -4,13 +4,14 @@ import subprocess
 from utils.text_tool import safe_decode
 from context import ctx_safe
 
-def format_text(s):
+def format_text(s, need_truncate=True):
     """ decode and truncate
 
     :s: str/bytes
     """
     s = safe_decode(s).strip()
-    s = ctx_safe.truncate_message(s).strip()
+    if need_truncate:
+        s = ctx_safe.truncate_message(s).strip()
     return s
 
 def build_env(d:dict=None):
@@ -24,9 +25,25 @@ def build_env(d:dict=None):
         env.update(d)
     return env
 
-def _format_return(t:tuple):
+def _need_whole_stdout(cmd_string:str):
+    """ some cases need stdout """
+
+    # curl wikipedia.org
+    if cmd_string.startswith("curl "):
+        for key in [
+            "wikipedia.org",
+        ]:
+            if key in cmd_string:
+                return True
+
+    return False
+
+def _format_return(cmd_string: str, t:tuple):
     """ truncate text for stdout and stderr """
-    return (t[0], format_text(t[1]), format_text(t[2]))
+    need_truncate = True
+    if _need_whole_stdout(cmd_string):
+        need_truncate = False
+    return (t[0], format_text(t[1], need_truncate=need_truncate), format_text(t[2]))
 
 def format_return(cmd_string:str, t:tuple):
     """ hack result """
@@ -40,9 +57,9 @@ def format_return(cmd_string:str, t:tuple):
             break
         if cmd_string.startswith(tool + " "):
             t = (t[0], t[1], "")
-            return _format_return(t)
+            return _format_return(cmd_string, t)
 
-    return _format_return(t)
+    return _format_return(cmd_string, t)
 
 def exec_cmd(cmd_string, no_need_stderr=False):
     """
