63b89f536f3d13b89ba58de2b0ce1bf434f5148c
DawsonLin <lin_dongsen@126.com>
2025-11-10 12:09:33 +0800

    bugfix: we MUST truncate large message for context safe

--- context/ctx_safe.py
+++ context/ctx_safe.py
@@ -9,10 +9,13 @@ from utils import print_tool
 from utils.thread_local_tool import get_agent_name
 
 MAX_MSG_SIZE = 3000
+LARGE_MSG_SIZE = 13000
 
 def is_need_truncate(msg_len:int):
     """ return bool. True is need truncate. """
     if get_agent_name() == "AgentWriter":
+        if msg_len > LARGE_MSG_SIZE:
+            return True
         return False
     if msg_len >= MAX_MSG_SIZE:
         return True
@@ -24,8 +27,6 @@ def truncate_message(msg):
     if is_need_truncate(len(msg)):
         print_tool.print_error(f"truncate message with the size: [{MAX_MSG_SIZE}]")
         suffix = " ... (force to truncate)"
-    else:
-        return msg
 
     if isinstance(msg, bytes):
         if suffix:
